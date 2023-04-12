4e7cd26099fa525261e7d992f965dcf0d104dfed
Pratik7759 <pratik682019@gmail.com>
2023-04-12 15:35:39 +0530

    fifth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'FormPOC';
+  signupForm: any = FormGroup;
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      firstname: new FormControl(null, [
+        Validators.required,
+        Validators.pattern('^[a-zA-Z][a-zA-Z ]+'),
+        Validators.minLength(3),
+      ]),
+      lastname: new FormControl(null, [
+        Validators.required,
+        Validators.pattern('^[a-zA-Z][a-zA-Z ]+'),
+        Validators.minLength(3),
+      ]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(
+          '(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-zd$@$!%*?&].{8,}'
+        ),
+      ]),
+      confirmpassword: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(
+          '(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-zd$@$!%*?&].{8,}'
+        ),
+      ]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm.value);
+    const status =
+      this.signupForm.password === this.signupForm.confirmpassword
+        ? 'equal'
+        : 'not equal';
+    console.log(status);
+  }
 }
